4ab19eedd290456c8a0a6c51cd1a2b78380a67dd
TWTimothylu <arway.lu@gmail.com>
2024-11-10 21:11:50 +0800

    照著ppt更新

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const tarotCards = [
     { name: '女祭司', image: 'images/High_Priestess.jpg', detailUrl: 'https://www.notion.so/11422e9535618072947ee383e13399cc?pvs=4' }
 ];
 
+const img = new Image();
+img.src = 'images/back.png';
+
 const card = document.getElementById('card');
 const cardName = document.getElementById('cardName');
 const readButton = document.getElementById('readButton');
 const drawCardButton = document.getElementById('drawCardButton');
+const pageTitle = document.getElementById('pageTitle');
+const subTitle = document.getElementById('subTitle');
+const slowBreathText = document.getElementById('slowBreathText');
 
 //const card = document.getElementById('card');
-const img = new Image();
-img.src = 'images/back.webp';
+
+
 
 img.onload = function() {
     const aspectRatio = img.naturalWidth / img.naturalHeight;
@@ -29,25 +35,25 @@ img.onload = function() {
 
 drawCardButton.addEventListener('click', () => {
     card.style.transform = 'rotateY(180deg)';
+    
     setTimeout(() => {
         const randomIndex = Math.floor(Math.random() * tarotCards.length);
         const selectedCard = tarotCards[randomIndex];
 
-        const img = new Image();
-        img.src = selectedCard.image;
+        card.style.background = `url('${selectedCard.image}') no-repeat center/contain`;
+        cardName.textContent = selectedCard.name;
+        cardName.style.display = 'block';
+        readButton.style.display = 'block';
 
-        img.onload = function() {
-            const aspectRatio = img.naturalWidth / img.naturalHeight;
-            const cardWidth = 300; // 設定寬度
-            const cardHeight = cardWidth / aspectRatio; // 根據比例自動計算高度
+        // 修改標題和副標題顏色
+        pageTitle.style.color = '#737373'; // 修改主標題顏色
+        subTitle.style.color = '#737373'; // 修改副標題顏色
+        //pageTitle.style.fontSize = "1.5em";
+        //subTitle.style.fontSize = "1.5em";
 
-            card.style.width = `${cardWidth}px`;
-            card.style.height = `${cardHeight}px`;
-            card.style.background = `url('${selectedCard.image}') no-repeat center/contain`;
-            cardName.textContent = selectedCard.name;
-            cardName.style.display = 'block';
-            readButton.style.display = 'block';
-        };
+        // 隱藏抽卡按鈕和提示文字
+        drawCardButton.style.display = 'none';
+        slowBreathText.style.display = 'none';
 
         readButton.onclick = () => {
             window.location.href = selectedCard.detailUrl;
